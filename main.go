201af62c9d214f432abdc7d415067a0a86f91b62
seedak <500052565@stu.upes.ac.in>
2019-06-24 14:55:48 +0530

    Added mongodb database to expense RestAPI. update and delete not working for now

--- main.go
+++ main.go
@@ -14,12 +14,33 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"upper.io/db.v3"
+	"upper.io/db.v3/mongo"
 )
 
-
+var settings = mongo.ConnectionURL{
+	Database:  `expense_db`,
+	Host:      `127.0.0.1`,
+}
+var sess db.Database
+var expenseCollection db.Collection
 var expenses types.Expenses
 
 func main() {
+	var err error
+	sess, err = mongo.Open(settings)
+	if err != nil {
+		log.Fatalf("db.Open(): %q\n", err)
+	}
+	defer sess.Close()
+
+	expenseCollection = sess.Collection("expense")
+
+	err = expenseCollection.Truncate()
+	if err != nil {
+		log.Fatalf("Truncate(): %q\n", err)
+	}
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
@@ -40,25 +61,25 @@ func main() {
 			r.Delete("/", DeleteExpense)
 		})
 	})
-
 	log.Fatal(http.ListenAndServe(":8080", r))
-
-
 }
 
 func ExpenseContext(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		expenseID := chi.URLParam(r, "id")
 		id, _:=strconv.Atoi(expenseID)
+		result := expenseCollection.Find()
+		var expenses types.Expenses
+		err := result.All(&expenses)
+		if err!= nil{
+			log.Println(err)
+		}
 		for _, expense := range expenses {
-
 			if expense.Id == id {
 				ctx := context.WithValue(r.Context(), "expense", expense )
 				next.ServeHTTP(w, r.WithContext(ctx))
 			}
 		}
-
 	})
 }
 
@@ -73,7 +94,11 @@ func CreateExpense(writer http.ResponseWriter, request *http.Request) {
 		log.Println(err)
 		return
 	}
-	expenses = append(expenses, *req.Expense)
+	_ , err = expenseCollection.Insert(*req.Expense)
+	if err != nil{
+		log.Println(err)
+		return
+	}
 	j, err := json.Marshal(req.Expense)
 	if err != nil{
 		render.Render(writer,request,ers.ErrRender(err))
@@ -84,6 +109,17 @@ func CreateExpense(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(http.StatusCreated)
 
 	_, _ = fmt.Fprintf(writer, `{"success": true, "data": %v}`, string(j))
+
+	// to check whether expense was added or not
+	//result := expenseCollection.Find()
+	//var print types.Expenses
+	//err = result.All(&print)
+	//if err!= nil{
+	//	log.Println(err)
+	//}
+	//for _, p := range print {
+	//	fmt.Println(p)
+	//}
 }
 func ListOneExpense(writer http.ResponseWriter, request *http.Request) {
 	expense := request.Context().Value("expense").(types.Expense)
@@ -95,7 +131,13 @@ func ListOneExpense(writer http.ResponseWriter, request *http.Request) {
 }
 
 func ListAllExpense(writer http.ResponseWriter, request *http.Request) {
-	err := render.Render(writer, request, response.ExpensesResponse(&expenses))
+	result := expenseCollection.Find()
+	var print types.Expenses
+	err := result.All(&print)
+	if err!= nil{
+		log.Println(err)
+	}
+	err = render.Render(writer, request, response.ExpensesResponse(&print))
 	if err != nil{
 		render.Render(writer,request,ers.ErrRender(err))
 		return
@@ -113,7 +155,21 @@ func UpdateExpense(writer http.ResponseWriter, request *http.Request) {
 		log.Println(err)
 		return
 	}
-	expenses[expense.Id-1] = *req.Expense
+	id := expense.Id
+	fmt.Println(id)
+	result := expenseCollection.Find("id", id)
+	err = result.One(&expense)
+	if err!= nil{
+		log.Println(err)
+	}
+	fmt.Println(result)
+	err = result.Update(*req.Expense)
+
+	//err = expenseCollection.UpdateReturning(*req.Expense)
+	if err!= nil{
+		log.Println(err)
+	}
+	//expenses[expense.Id-1] = *req.Expense
 
 
 	err = render.Render(writer, request, response.Listexpense(&expense))
